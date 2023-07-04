be6a94a36faebfd440e4349b6b76679c2ff50bd4
TaeYoon Kim <xodbsrla1025@naver.com>
2023-07-04 14:36:20 +0900

    Update: "ch03/ch03_3"

---
+++ ch03/ch03_3/src/pages/DisplayNoneTest.tsx
@@ -0,0 +1,14 @@
+import {Title} from '../components'
+
+export default function CopyMe() {
+  return (
+    <section className="mt-4">
+      <Title>DisplayNoneTest</Title>
+      <div className="mt-4">
+        <p className="visible">visibility: visible text</p>
+        <p className="invisible">visibility: hidden text</p>
+        <p className="hidden">visibility: none text</p>
+      </div>
+    </section>
+  )
+}
